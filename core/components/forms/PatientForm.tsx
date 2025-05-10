e9145c83c6a018872e890c47983ae2bd1d272391
muhammadderic <muhammad.donny.e@gmail.com>
2025-05-10 11:11:11 +0700

    f: update home page and add initial Patient Form compt

---
+++ core/components/forms/PatientForm.tsx
@@ -0,0 +1,10 @@
+export const PatientForm = () => {
+  return (
+    <form className="flex-1 space-y-6">
+      <section className="mb-12 space-y-4">
+        <h1 className="header">Hi there 👋</h1>
+        <p className="text-dark-700">Get started with appointments.</p>
+      </section>
+    </form>
+  )
+}
